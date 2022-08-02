eaf70d734fc85251ea7ef98a57796866bb7ecdb3
mithun12r <110412805+mithun12r@users.noreply.github.com>
2022-08-02 17:36:24 +0530

    Add files via upload

---
+++ orderpage.java
@@ -0,0 +1,43 @@
+package project2;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class orderpage {
+	
+	@Test()
+    public void Order() throws InterruptedException
+    {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\mithun.r\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://mobileworld.azurewebsites.net");
+        driver.manage().window().maximize();
+        //AllMobiles
+        driver.findElement(By.xpath("//a[@href='mobile.html']")).click();
+        Thread.sleep(1000);
+        JavascriptExecutor js=(JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,500)");
+        driver.findElement(By.xpath("//tbody[@id='myTable']/tr[2]/td[5]/a")).click();
+        Set<String> windows = driver.getWindowHandles();
+        Iterator<String> it = windows.iterator();
+        String parentId = it.next();
+        String childId = it.next();
+        driver.switchTo().window(childId);
+        driver.findElement(By.xpath("//input[@id='inputFirstName']")).sendKeys("mithun");
+        driver.findElement(By.xpath("//body/div[@class='container']/div[1]/div[2]/form[1]/div[1]/div[2]/input")).sendKeys("gowda");
+        driver.findElement(By.id("inputEmail")).sendKeys("mithun2504@gmail.com");
+        driver.findElement(By.id("inputPassword")).sendKeys("mithun@123");
+        driver.findElement(By.id("flexRadioDefault1")).click();
+        driver.findElement(By.xpath("//input[@type='number']")).sendKeys("8884833136");
+        driver.findElement(By.id(" address1")).sendKeys("Hirisave");
+        driver.findElement(By.id("address2")).sendKeys("Channarayapatna");
+        driver.findElement(By.id("inputCity")).sendKeys("Hassan");
+
+    }
+}
